95fdb594ffd185f10c8479dc47e97ef85f07f57c
NIMIDEV <99932058+NIMICODER@users.noreply.github.com>
2022-12-12 04:16:56 +0100

    Add project files.

---
+++ AsynchronousProgramming/Calculations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    public class Calculations
+    {
+        public async static void Test()
+        {
+            await Calculate();
+        }
+
+        public async static Task Calculate() 
+        {
+
+            await Calculate1_2();
+            Calculate3();   
+        }   
+
+        public async static Task Calculate1_2()
+        {
+           var result1 = await Task.Run(() => {
+                return Calculate1();
+            });
+
+            Calculate2(result1);
+        }
+
+        public static int Calculate1()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Calculating result1");
+            return 100;   
+        }
+
+        public static int Calculate2(int result1)
+        {
+           // Thread.Sleep(1000);
+            Console.WriteLine("Calculating result2");
+            return result1*2;
+        }
+
+        public static int Calculate3()
+        {
+           // Thread.Sleep(1000);
+            Console.WriteLine("Calculating result3");
+            return 300;
+        }
+    }   
+}
+ 
---
+++ AsynchronousProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AsynchronousProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Calculations calculations = new Calculations();
+            Calculations.Test();
+            Console.Read();
+        }
+    }
+}
